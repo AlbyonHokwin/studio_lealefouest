c8c8859f4218615dcc2039567f970c6204176110
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-09 18:06:26 +0100

    reorganize import and change catchPhrase field to introductoryPhrase

--- schemas/Pages/home.ts
+++ schemas/Pages/home.ts
@@ -1,4 +1,5 @@
 import { defineType, defineField, defineArrayMember } from "sanity";
+import type { ArrayRule, PortableTextTextBlock, PortableTextSpan } from "sanity";
 
 export default defineType({
     name: 'home',
@@ -11,17 +12,38 @@ export default defineType({
         }),
 
         defineField({
-            name: 'catchPhrases', type: 'array', title: 'Phrases d\'accroche',
+            name: 'introductory', type: 'array', title: 'Phrase d\'accueil',
             of: [defineArrayMember({
-                name: 'part',
-                type: 'object',
-                title: 'Partie',
-                fields: [
-                    defineField({ type: 'string', name: 'text' }),
-                    defineField({ type: 'string', name: 'font' })
-                ]
-            })]
+                type: 'block',
+                styles: [],
+                lists: [],
+                marks: {
+                    decorators: [{ title: 'Emphasis', value: 'em' }],
+                    annotations: []
+                }
+            })],
+            validation: (Rule: ArrayRule<PortableTextTextBlock[]>) => Rule.required().custom((blocks) => {
+                if (!blocks || !blocks[0]) return true;
+
+                const children = blocks[0].children as PortableTextSpan[];
+                const numberOfEmphasis: number = children.reduce((a, c) => {
+                    if (!c.marks) return a;
+                    return c.marks.includes('em') ? a + 1 : a;
+                }, 0)
+
+                if (numberOfEmphasis > 1) return 'Une seule partie doit être accentuée';
+
+                if (numberOfEmphasis === children.length) return 'Ne pas accentuer tout le texte';
+
+                return true
+            }).error()
         }),
+
+        defineField({
+            name: 'picture', type: 'reference', title: 'Image',
+            to: [{ type: 'accessibleImage' }],
+            validation: Rule => Rule.required(),
+        })
     ],
     preview: {
         select: {
