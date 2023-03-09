fc0b612c093d94c86ee7dfc3a5aeb5226e55de75
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-09 18:05:33 +0100

    create normalSchemas object

--- schemas/index.ts
+++ schemas/index.ts
@@ -5,9 +5,21 @@ import component from "./Objects/component";
 import accessibleImage from "./Objects/accessibleImage";
 import icon from "./Objects/icon";
 
-export const singletonSchemas = [
+type Schema = {
+    typeName: string;
+    title: string;
+}
+
+export const singletonSchemas: Schema[] = [
     { typeName: 'home', title: 'Accueil' },
     { typeName: 'why', title: 'Pourquoi' },
+];
+
+export const normalSchemas: Schema[] = [
+    { typeName: 'profile', title: 'Profil' },
+    { typeName: 'component', title: 'Composant' },
+    { typeName: 'accessibleImage', title: 'Image' },
+    { typeName: 'icon', title: 'Icône' },
 ];
 
 export const singletonTypes = new Set(singletonSchemas.map(s => s.typeName));
