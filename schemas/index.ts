2f7e6f8b98e3bdbf58f891d7508fc82da324e488
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-09 17:48:32 +0100

    add created schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,14 @@
-export const schemaTypes = []
+import profile from "./profile";
+import home from "./Pages/home";
+import why from "./Pages/why";
+import component from "./Objects/component";
+import accessibleImage from "./Objects/accessibleImage";
+import icon from "./Objects/icon";
+
+export const singletonSchemas = [
+    { typeName: 'home', title: 'Accueil' },
+    { typeName: 'why', title: 'Pourquoi' },
+];
+
+export const singletonTypes = new Set(singletonSchemas.map(s => s.typeName));
+export const schemaTypes = [home, why, profile, component, accessibleImage, icon];
